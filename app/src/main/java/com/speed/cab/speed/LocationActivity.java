cca16054d392ef5bcd15f35fb0ae8a249d095198
Chandrasekar <vignc@ac87a319449b.ant.amazon.com>
2016-07-11 14:32:00 +0530

    Printing Address, Latitude,Longitude

--- app/src/main/java/com/speed/cab/speed/LocationActivity.java
+++ app/src/main/java/com/speed/cab/speed/LocationActivity.java
@@ -2,8 +2,19 @@ package com.speed.cab.speed;
 
 import android.*;
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -11,85 +22,242 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class LocationActivity extends FragmentActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
+public class LocationActivity extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        OnMapReadyCallback {
 
-    private GoogleMap map;
-    private static LatLng myLatlng;
-    private int counter =0;
+    GoogleApiClient mGoogleApiClient;
+    GoogleMap mGoogleMap;
+    SupportMapFragment mFragment;
+
     Button postButton;
+    ImageView centerMarker;
+    LatLng userLocation, centerPosition;
+    TextView mLocationMarkerText;
+
+    private AddressResultReceiver mResultReceiver;
+    /**
+     * The formatted location address.
+     */
+    protected String mAddressOutput;
+    protected String mAreaOutput;
+    protected String mCityOutput;
+    protected String mStateOutput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        mLocationMarkerText = (TextView) findViewById(R.id.locationMarkertext);
+        postButton = (Button) findViewById(R.id.postButton);
+        mFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mFragment.getMapAsync(this);
+    }
 
-        postButton= (Button) findViewById(R.id.postButton);
+    private void zoomToLocation(LatLng location, int zoomValue) {
+        if (location != null) {
+            //get Current Location and zoom to location
+            mGoogleMap.clear();
+            //mCurrLocation = mGoogleMap.addMarker(markerOptions);
+            CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(location, zoomValue);
+            mGoogleMap.animateCamera(yourLocation);
+        }
+    }
 
-        mapFragment.getMapAsync(this);
-        postButton.setOnClickListener(new View.OnClickListener() {
+    public LatLng getUserLocation() {
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        return new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mGoogleMap = googleMap;
+
+        mGoogleMap.setMyLocationEnabled(true);
+        mResultReceiver = new AddressResultReceiver(new Handler());
+        buildGoogleApiClient();
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //Unregister for location callbacks:
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        Toast.makeText(this, "buildGoogleApiClient", Toast.LENGTH_SHORT).show();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    protected void startIntentService(Location mLocation) {
+        // Create an intent for passing to the intent service responsible for fetching the address.
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+        Log.d("Intent Service", "StartIntentService");
+        // Pass the result receiver as an extra to the service.
+        intent.putExtra(AppUtils.LocationConstants.RECEIVER, mResultReceiver);
+
+        // Pass the location data as an extra to the service.
+        intent.putExtra(AppUtils.LocationConstants.LOCATION_DATA_EXTRA, mLocation);
+
+        // Start the service. If the service isn't already running, it is instantiated and started
+        // (creating a process for it if needed); if it is running then it remains running. The
+        // service kills itself automatically once all intents are processed.
+        startService(intent);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this, "onConnected", Toast.LENGTH_SHORT).show();
+        zoomToLocation(getUserLocation(), 15);
+        cameraListenerModule();
+        //LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "onConnectionSuspended", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private boolean checkLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(LocationActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    private void addMarkerOnCustomLocation(LatLng customLatLng, Marker newMarker) {
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(customLatLng);
+        newMarker = mGoogleMap.addMarker(markerOptions);
+    }
+
+    private void cameraListenerModule() {
+        mGoogleMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
             @Override
-            public void onClick(View view) {
-                if(myLatlng!=null){
-                    postButton.setText(myLatlng.toString());
+            public void onCameraChange(CameraPosition cameraPosition) {
+                Log.d("Camera postion change" + "", cameraPosition + "");
+                centerPosition = cameraPosition.target;
+                mGoogleMap.clear();
+                try {
+                    Location mLocation = new Location("");
+                    mLocation.setLatitude(centerPosition.latitude);
+                    mLocation.setLongitude(centerPosition.longitude);
+                    startIntentService(mLocation);
+                    Log.d("Button Text", centerPosition.toString());
+                    postButton.setText(centerPosition.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+            }
+        });
+        mGoogleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                locationButtonClickHandler();
+                return true;
             }
         });
     }
 
+    private void locationButtonClickHandler() {
+        Log.d("LocationButtonListener", "It works");
+        userLocation = getUserLocation();
+        Location mLocation = new Location("");
+        mLocation.setLatitude(userLocation.latitude);
+        mLocation.setLongitude(userLocation.longitude);
+        startIntentService(mLocation);
+        zoomToLocation(userLocation, 15);
+        //updateCameraToLocation(userLocation);
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private void updateCameraToLocation(LatLng latLng) {
+        Log.d("LocationButtonListener", "updateCameraLocation");
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 20);
+        mGoogleMap.animateCamera(cameraUpdate);
+    }
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-        final Marker marker = map.addMarker(new MarkerOptions().position(new LatLng(12,77)));
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            map.setMyLocationEnabled(true);
-            Log.d("onMapReady", Integer.toString(counter++));
-            map.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+    public void onLocationChanged(Location location) {
 
-                @Override
-                public void onMyLocationChange(Location arg0) {
-                    // TODO Auto-generated method stub
-                    myLatlng = new LatLng(arg0.getLatitude(), arg0.getLongitude());
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatlng, 15));
-                    marker.setPosition(myLatlng);
-                    Log.d("LatLong", myLatlng.toString());
-                    // Zoom in, animating the camera.
-                }
-            });
+    }
 
+    class AddressResultReceiver extends ResultReceiver {
+        public AddressResultReceiver(Handler handler) {
+            super(handler);
         }
-        else {
-            // Show rationale and request permission.
-            ActivityCompat.requestPermissions(LocationActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    1);
+
+        /**
+         * Receives data sent from FetchAddressIntentService and updates the UI in MainActivity.
+         */
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            // Display the address string or an error message sent from the intent service.
+            mAddressOutput = resultData.getString(AppUtils.LocationConstants.RESULT_DATA_KEY);
+
+            mAreaOutput = resultData.getString(AppUtils.LocationConstants.LOCATION_DATA_AREA);
+
+            mCityOutput = resultData.getString(AppUtils.LocationConstants.LOCATION_DATA_CITY);
+            mStateOutput = resultData.getString(AppUtils.LocationConstants.LOCATION_DATA_STREET);
+
+            displayAddressOutput();
+            Log.d("AddressOutput", "Function called");
+            // Show a toast message if an address was found.
+            if (resultCode == AppUtils.LocationConstants.SUCCESS_RESULT) {
+                //  showToast(getString(R.string.address_found));
+            }
         }
-        // Move the camera instantly to Sydney with a zoom of 15.
 
+    }
 
-        // Construct a CameraPosition focusing on Mountain View and animate the camera to that position
+    protected void displayAddressOutput() {
+        //  mLocationAddressTextView.setText(mAddressOutput);
+        try {
+            // mLocationText.setText(mAreaOutput+ "");
+            mLocationMarkerText.setText(mAddressOutput);
+            Log.d("Address : ", mAddressOutput);
+            //mLocationText.setText(mAreaOutput);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
